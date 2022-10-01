8b0b20080f033c973e44c834b3845b5afdc81d00
Ivo Lekov <lekov.external@intralot.com>
2022-10-01 19:45:51 +0300

    Fix db relations.

--- ShopingList/Migrations/20221001160401_Init.Designer.cs
+++ ShopingList/Migrations/20221001160401_Init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopingList.Data;
 
@@ -11,9 +12,10 @@ using ShopingList.Data;
 namespace ShopingList.Migrations
 {
     [DbContext(typeof(ShopingListDBContext))]
-    partial class ShopingListDBContextModelSnapshot : ModelSnapshot
+    [Migration("20221001160401_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -237,9 +239,6 @@ namespace ShopingList.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
-
-                    b.Property<DateTime>("TimeStamp")
-                        .HasColumnType("datetime2");
 
                     b.Property<string>("Title")
                         .IsRequired()
